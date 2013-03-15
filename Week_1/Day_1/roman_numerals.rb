9b68d6c56e6f4d4581d2ced03dd5334ca7c3764c
John Figueiredo <jfigueir33@gmail.com>
2013-03-15 11:51:55 -0700

    roman numerals

--- Week_1/Day_1/roman_numerals.rb
+++ Week_1/Day_1/roman_numerals.rb
@@ -1,16 +1,75 @@
 def roman_numerals(number)
-	if number >= 1000
-	elsif number < 1000 && number >= 500
-	elsif number < 500 && number >= 100
-	elsif number < 100 && number >= 50
-	elsif number < 50 && number >= 10
-	elsif number < 10 && number >= 5
-	else
-		ones = remainder
+		
+		thousand = number
+		while thousand >= 1000
+			print "M"
+			thousand -= 1000
+		end
+
+		thousand_remainder = thousand
+
+		five_hundred = thousand_remainder
+		while five_hundred >= 500
+			print "D"
+			five_hundred -= 500
+		end
+
+		five_hundred_remainder = five_hundred
+
+		hundred = five_hundred_remainder
+		while hundred >= 100
+			print "C"
+			hundred -= 100
+		end
+
+		hundred_remainder = hundred
+
+		fifty = hundred_remainder
+		while fifty >= 50
+			print "L"
+			fifty -= 50
+		end
+
+		fifty_remainder = fifty
+
+		tens = fifty_remainder
+		while tens >= 10
+			print "X"
+			tens -= 10
+		end
+
+		tens_remainder = tens
+
+		fives = tens
+		while fives >= 5
+			print "V"
+			fives -= 5
+		end
+
+		fives_remainder = fives
+
+		ones = fives_remainder #tens_remainder
 		while ones > 0 
 			print "I"
 			ones -= 1
 		end	
+		puts ''
 end
 
+
 roman_numerals(4)
+
+roman_numerals(7)
+
+roman_numerals(17)
+
+roman_numerals(67)
+
+roman_numerals(167)
+
+roman_numerals(667)
+
+roman_numerals(1100)
+
+
+roman_numerals(1667)
