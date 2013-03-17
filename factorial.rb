134b64cb6d317c317919d1906c63904e77b68f63
John Figueiredo <jfigueir33@gmail.com>
2013-03-17 13:28:59 -0700

    understanding recursion

--- factorial.rb
+++ factorial.rb
@@ -13,15 +13,11 @@ puts factorial_iterative(5)
 
 puts ''
 
-def factorial_recursive(number)
-	product = 1
-	while number > 1
-		product *= number
-		number -= 1
-	end
-	product
+def factorial_recursive(n)
+	return  n  if n <= 1 
+  factorial_recursive( n - 1 ) * n
 end
 
 puts factorial_recursive(3)
 puts factorial_recursive(4)
-puts factorial_recursive(5)
+puts factorial_recursive(10)
